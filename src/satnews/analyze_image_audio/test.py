15c51bf75f46c0bcd23abbe9f9b1e9568dceab49
Pranav Yadav <102318334+pra-nav-04@users.noreply.github.com>
2025-07-01 21:43:50 +0200

    Add files via upload

---
+++ src/satnews/analyze_image_audio/test.py
@@ -0,0 +1,10 @@
+# test.py
+
+import analyze_image_audio
+
+# Replace these paths with your actual files:
+image_path = r"D:\Downloads\\example.png"
+audio_path = r"D:\Downloads\\example.mp3"
+summarize = True   # Set to False if you don't want summarization
+
+analyze_image_audio.main(image_path, audio_path, summarize)
